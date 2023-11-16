eceeec91777e04ca578f8ba1f0ef4dbfea2b9cc7
Ali <ali.arioglu06@gmail.com>
2023-11-16 14:51:37 +0300

    List update functionality in List Header component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { ListWithCards } from "@/types";
+import { useState, useEffect } from "react";
+
 import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
+import { ListWithCards } from "@/types";
 
 interface ListContainerProps {
   boardId: string;
@@ -9,10 +12,19 @@ interface ListContainerProps {
 }
 
 export const ListContainer = ({ boardId, data }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState(data);
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
-      <div className="flex-shrink-0 w-1" />
+    <ol className="flex gap-x-3 h-full">
+      {orderedData.map((list, index) => (
+        <ListItem key={list.id} index={index} data={list} />
+      ))}
       <ListForm />
+      <div className="flex-shrink-0 w-1" />
     </ol>
   );
 };
