58d76fff7ff1ad3d0f15c65aa6e3dccb2714793f
Ali <ali.arioglu06@gmail.com>
2023-11-16 17:29:48 +0300

    card form

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { ElementRef, useRef, useState } from "react";
+
 import { ListWithCards } from "@/types";
 import { ListHeader } from "./list-header";
+import { CardForm } from "./card-form";
+import { cn } from "@/lib/utils";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -9,10 +14,42 @@ interface ListItemProps {
 }
 
 export const ListItem = ({ data, index }: ListItemProps) => {
+  const textAreaRef = useRef<ElementRef<"textarea">>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textAreaRef.current?.focus();
+    });
+  };
+
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} />
+        <ListHeader data={data} onAddCard={enableEditing} />
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.cards.length > 0 ? "mt-2" : "mt-0"
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem key={card.id} index={index} data={card} />
+          ))}
+        </ol>
+        <CardForm
+          listId={data.id}
+          ref={textAreaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
